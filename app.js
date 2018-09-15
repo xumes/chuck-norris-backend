be96fb44d73fb8cd902454fa8bbd818573d3acd6
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-15 12:44:58 -0300

    exemplo agora usando o Express

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express')
+
+const app = express()
+
+const hostname = '127.0.0.1';
+const port = 3000
+
+const listaClientes = [
+    {
+        nome: 'Xumes',
+        cargo: 'dev'
+    },
+    {
+        nome: 'Robinson',
+        cargo: 'adv'
+    },
+    {
+        nome: 'Brian',
+        cargo: 'tecladinho'
+    },
+    {
+        nome: 'Reginaldo',
+        cargo: 'professor'
+    },
+    {
+        nome: 'Marcelo',
+        cargo: 'aluno'
+    }
+]
+
+const oCara = {
+    nome: 'Robinson',
+        cargo: 'adv'
+}
+
+app.get('/', (req, res) => {
+    res.send("Home Page")
+})
+
+app.get('/clientes', (req, res) => {
+    res.send(listaClientes)
+})
+
+app.get('/xumes', (request, response) => {
+    response.json(oCara)
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Serveris running in http://${hostname}:${port} `)
+});
+
+
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = process.env.PORT || 3000;
+
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
