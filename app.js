4a15f54dec66acfa3fbd901c8bae0f082559b82d
Reginaldo Santos <rmsantos@br.ibm.com>
2018-09-15 14:06:20 -0300

    corrigindo erro

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/le-piada', (req, res) => {
         })
         .catch((err) => {
             console.log(err)
-            res.send(resultado)
+            res.send(err)
         })
     
 })
